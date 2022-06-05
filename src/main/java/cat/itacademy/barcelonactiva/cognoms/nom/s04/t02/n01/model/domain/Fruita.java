fc5edc374a4c8486f2beb2fe0771a644dda17d13
4SH1N <20546366+haustasis@users.noreply.github.com>
2022-06-05 18:40:09 +0200

    Fruita init

---
+++ src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t02/n01/model/domain/Fruita.java
@@ -0,0 +1,51 @@
+package cat.itacademy.barcelonactiva.cognoms.nom.s04.t02.n01.model.domain;
+
+public class Fruita {
+
+	private int id;
+	private String nom;
+	private int quantitatQuilos;
+	
+	public Fruita() {
+		
+	}
+
+	public Fruita(int id, String nom, int quantitatQuilos) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.quantitatQuilos = quantitatQuilos;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public int getQuantitatQuilos() {
+		return quantitatQuilos;
+	}
+
+	public void setQuantitatQuilos(int quantitatQuilos) {
+		this.quantitatQuilos = quantitatQuilos;
+	}
+
+	@Override
+	public String toString() {
+		return "Fruita [id=" + id + ", nom=" + nom + ", quantitatQuilos=" + quantitatQuilos + "]";
+	}
+	
+	
+	
+}
