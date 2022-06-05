dbfd193068c8b0b9e811f4f58f91a9b48a1ec3c8
4SH1N <20546366+haustasis@users.noreply.github.com>
2022-06-05 17:54:38 +0200

    springboard

---
+++ src/main/java/cat/itacademy/barcelonactiva/arisogorostizaga/toni/s04/t02/n01/S04T02N01ArisoGorostizagaToniApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.arisogorostizaga.toni.s04.t02.n01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T02N01ArisoGorostizagaToniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T02N01ArisoGorostizagaToniApplication.class, args);
+	}
+
+}
