712ab7e9b15d3f1e19ef6c27f9b943d965f683d9
4SH1N <20546366+haustasis@users.noreply.github.com>
2022-06-05 18:58:24 +0200

    Fruita Repo

---
+++ src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t02/n01/model/repository/FruitaRepository.java
@@ -0,0 +1,10 @@
+package cat.itacademy.barcelonactiva.cognoms.nom.s04.t02.n01.model.repository;
+
+import cat.itacademy.barcelonactiva.cognoms.nom.s04.t02.n01.model.domain.Fruita;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FruitaRepository extends JpaRepository<Fruita, Integer> {
+	  List<Fruita> trobaFruitaNom(String nom);
+	}
